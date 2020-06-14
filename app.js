913bc075428db731bd32e754c6d1b2434b38120a
Girish Koundinya <girishgkoundinya@gmail.com>
2020-06-14 11:26:18 +0530

    Added mailer code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const { Telegraf } = require('telegraf')
-
+const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.start(function(cxt){
+    ctx.reply('Welcome!');
+});
+
+bot.hears("hi", function (ctx){
+    const mailer = require('./mailer')
+    mailer.sendEmail();
+    ctx.reply('Hey there')
+});
+
 bot.launch()
