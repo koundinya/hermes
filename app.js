36a54a498b2771d3fc9b1c52b0c3342ca0cfb176
Girish Koundinya <girishgkoundinya@gmail.com>
2020-08-08 11:59:27 +0530

    port

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ function verifyUsername(name){
 const PORT = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT | 3000
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
