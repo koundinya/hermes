c4f449301c63933eaf860d098b898ee1760d4280
Girish Koundinya <girishgkoundinya@gmail.com>
2020-08-08 11:10:11 +0530

    added mailer

--- mailer.js
+++ mailer.js
@@ -15,7 +15,7 @@ function sendEmail(article,source,destination){
     attachments: [
       {
         content: Buffer.from(content).toString('base64'),
-        filename: article.title,
+        filename: article.title + ".html",
         type: "text/html",
         disposition: "attachment"
       }
